45874fb46347c80a625be57bd11660a41e500725
ngxpaul <phongnx.22git@vku.udn.vn>
2024-03-14 15:42:33 +0700

    244. Custom Hook: Managing State & Returning State Values

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+export default function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch user places." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+  return { isFetching, error, fetchedData };
+}
